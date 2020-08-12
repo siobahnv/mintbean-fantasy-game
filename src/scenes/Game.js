936f372384ee6bb9e412617e5f8c3097bc57a9ea
Heather Ransome <siobahnv@gmail.com>
2020-08-12 15:10:25 -0700

    Playing with particles.

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -6,7 +6,8 @@ import star from "../assets/star.png";
 
 import dragon from "../assets/sad-dragon200.png";
 import butterfly from "../assets/butterfly.png";
-import fire from "../assets/flames.png";
+import flashes from "../assets/muzzleflash3.png";
+import tears from "../assets/blue.png";
 
 import { accelerate, decelerate } from "../utils";
 
@@ -28,7 +29,6 @@ export default new Phaser.Class({
       frameHeight: 231
     });  
 
-    this.load.image("fire", fire);
   },
 
   create: function create() {
@@ -36,11 +36,101 @@ export default new Phaser.Class({
 
     cursors = this.input.keyboard.createCursorKeys();
 
-    flames = this.physics.add.image(400, 200, "fire");
-
     dragonbox = this.physics.add.image(400, 300, "dragon", 15);
     dragonbox.setBounce(0.2);
     dragonbox.setCollideWorldBounds(true);
+
+    var particles = this.add.particles(tears);
+
+    var emitter = particles.createEmitter({
+      // **basic properties of particles**
+      // **initial position**
+      x: 0,             // { min, max }, or { min, max, steps }
+      y: 0,             // { min, max }, or { min, max, steps }
+      // follow: null,
+      // followOffset: {
+      //    x: 0,
+      //    y: 0
+      // },
+      // **emit zone**
+      // emitZone: {
+      //     type: 'random',    // 'random', or 'edge'
+      //     source: geom,      // Geom like Circle, or a Path or Curve
+      //     **type = edge**
+      //     quantity: 1,
+      //     stepRate: 0,
+      //     yoyo: false,
+      //     seamless: true
+      // },
+  
+      // **target position**
+      // moveToX:          // { min, max }, or { min, max, steps }
+      // moveToY:          // { min, max }, or { min, max, steps }
+      // **death zone**
+      // deathZone: {
+      //      type: 'onEnter',  // 'onEnter', or 'onLeave'
+      //      source: geom      // Geom like Circle or Rect that supports a 'contains' function
+      // }
+  
+      // **angle**
+      // radial: true,
+      // angle: { min: 0, max: 360 },  // { start, end, steps }
+  
+      // **scale**
+      // scale: 1,             // { start, end },
+      // scaleX: 1,
+      // scaleY: 1,
+  
+      // **render**
+      // frame:                // one or more texture frames, or a configuration object.
+      // alpha: 1,             // { min, max }
+      // visible: true,
+      // tint: 0xffffffff,     // a number 0xfffffff, or an array [ 0xffff00, 0xff0000, 0x00ff00, 0x0000ff ]
+      blendMode: 'ADD',  // Phaser.BlendModes
+  
+      // delay: 0,
+      lifespan: 1000,       // { min, max }, or { min, max, steps }
+  
+  
+      // **physics**
+      speed: { min: 400, max: 600 },  // { min, max }, or { min, max, steps }
+      // speedX:               // { min, max }, or { min, max, steps }
+      // speedY:               // { min, max }, or { min, max, steps }
+      // gravityX:
+      gravityY: 300,
+      // accelerationX:
+      // accelerationY:
+      // maxVelocityX: 10000,
+      // maxVelocityY: 10000,
+  
+      // **bounce**
+      // bounce: 0,
+      // bounds: nul,           // Phaser.Geom.Rectangle, or { x, y, width, height }
+      // collideBottom: true,
+      // collideTop: true,
+      // collideLeft: true,
+      // collideRight : true,
+  
+      // **callback**
+      // emitCallback: null,
+      // emitCallbackScope: null,
+      // deathCallback: null,
+      // deathCallbackScope: null,
+  
+      // **custom particle**
+      // particleClass: Phaser.GameObjects.Particles.Particle
+  
+      // **emitter**
+      // name: '',
+      // on: true,          // set false to stop emitter
+      // active: true,      // set false to pause emitter and particles
+      // frequency: 0,      // -1 for exploding emitter
+      quantity: 1,       // { min, max }
+      // maxParticles: 0,
+      // rotate: 0,         // { start, end }, or { start, end, ease },
+      // timeScale: 1,
+  
+  });
 
   },
   update: function () {
