fdfacaee362b88f35039e7067e01d872c0ea5386
Heather Ransome <siobahnv@gmail.com>
2020-08-12 16:35:03 -0700

    Last attempt at sprites for today.

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -14,6 +14,9 @@ import { accelerate, decelerate } from "../utils";
 let dragonbox;
 let cursors;
 let flames;
+// let anim;
+let rain;
+// let image;
 
 export default new Phaser.Class({
   Extends: Phaser.Scene,
@@ -29,10 +32,19 @@ export default new Phaser.Class({
       frameHeight: 231
     });  
 
+    console.log("Tears being defined...");
+    // this.load.spritesheet('tears', tears, { frameWidth: 72, frameHeight: 72 });
+    this.load.image("tears", tears);
+    console.log(tears);
   },
 
   create: function create() {
     this.add.image(400, 300, "background");
+    // this.physics.startSystem(Phaser.Physics.ARCADE);
+    // this.world.enableBody = true;
+    // this.physics.startSystem(Phaser.Physics.ARCADE);
+    // image = this.add.sprite(500, 300, 'tears');
+    // this.physics.enable(image, Phaser.Physics.ARCADE);
 
     cursors = this.input.keyboard.createCursorKeys();
 
@@ -40,7 +52,23 @@ export default new Phaser.Class({
     dragonbox.setBounce(0.2);
     dragonbox.setCollideWorldBounds(true);
 
-    var particles = this.add.particles(tears);
+    // let config = {
+    //   key: 'rain',
+    //   frames: this.anims.generateFrameNumbers('tears'),
+    //   frameRate: 18,
+    //   repeat: -1
+    // };
+
+    // anim = this.anims.create(config);
+
+    // var sprite = this.add.sprite(72, 72, 'tears');
+    // sprite.animations.add('fall');
+    // sprite.animations.play('fall', 50, true);
+
+    console.log("Moar tears: ", tears);
+    rain = this.physics.add.sprite('"../assets/bluesprite.png"');
+
+    var particles = this.add.particles(rain);
 
     var emitter = particles.createEmitter({
       // **basic properties of particles**
@@ -82,9 +110,9 @@ export default new Phaser.Class({
       // scaleY: 1,
   
       // **render**
-      // frame:                // one or more texture frames, or a configuration object.
+      // frame: 0,                // one or more texture frames, or a configuration object.
       // alpha: 1,             // { min, max }
-      // visible: true,
+      visible: true,
       // tint: 0xffffffff,     // a number 0xfffffff, or an array [ 0xffff00, 0xff0000, 0x00ff00, 0x0000ff ]
       blendMode: 'ADD',  // Phaser.BlendModes
   
@@ -104,9 +132,9 @@ export default new Phaser.Class({
       // maxVelocityY: 10000,
   
       // **bounce**
-      // bounce: 0,
+      bounce: 0,
       // bounds: nul,           // Phaser.Geom.Rectangle, or { x, y, width, height }
-      // collideBottom: true,
+      collideBottom: true,
       // collideTop: true,
       // collideLeft: true,
       // collideRight : true,
@@ -118,13 +146,13 @@ export default new Phaser.Class({
       // deathCallbackScope: null,
   
       // **custom particle**
-      // particleClass: Phaser.GameObjects.Particles.Particle
+      particleClass: Phaser.GameObjects.Particles.Particle,
   
       // **emitter**
       // name: '',
       // on: true,          // set false to stop emitter
       // active: true,      // set false to pause emitter and particles
-      // frequency: 0,      // -1 for exploding emitter
+      frequency: 0,      // -1 for exploding emitter
       quantity: 1,       // { min, max }
       // maxParticles: 0,
       // rotate: 0,         // { start, end }, or { start, end, ease },
@@ -146,5 +174,7 @@ export default new Phaser.Class({
     if (cursors.right.isDown) dragonbox.setVelocityX(accelerate(velocity.x, 1));
     if (cursors.down.isDown) dragonbox.setVelocityY(accelerate(velocity.y, 1));
     if (cursors.left.isDown) dragonbox.setVelocityX(accelerate(velocity.x, -1));
+
+    // this.physics.arcade.collide(rain, dragonbox);
   }
 });
