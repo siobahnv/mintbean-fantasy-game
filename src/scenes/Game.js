57da34d3bc0444bfbbfb08a41c19ca99f740197d
Heather Ransome <siobahnv@gmail.com>
2020-08-12 11:44:29 -0700

    Sorta working assets.

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -2,11 +2,17 @@ import Phaser from "phaser";
 import mp3 from "../assets/Orbital\ Colossus.mp3";
 import background from "../assets/scifi_platform_BG1.jpg";
 import tiles from "../assets/scifi_platformTiles_32x32.png";
-import star from "../assets/star.png"
+import star from "../assets/star.png";
+
+import dragon from "../assets/sad-dragon200.png";
+import butterfly from "../assets/butterfly.png";
+import fire from "../assets/flames.png";
+
 import { accelerate, decelerate } from "../utils";
 
-let box;
+let dragonbox;
 let cursors;
+let flames;
 
 export default new Phaser.Class({
   Extends: Phaser.Scene,
@@ -17,67 +23,38 @@ export default new Phaser.Class({
   preload: function preload() {
     this.load.image("background", background);
 
-    this.load.spritesheet('tiles', tiles, {
-      frameWidth: 32,
-      frameHeight: 32
-    });
+    this.load.spritesheet('dragon', dragon, {
+      frameWidth: 200,
+      frameHeight: 231
+    });  
 
-    this.load.image("star", star);
+    this.load.image("fire", fire);
   },
+
   create: function create() {
     this.add.image(400, 300, "background");
 
-    const stars = this.physics.add.group({
-      key: 'star',
-      repeat: 11,
-      setScale: {x: 0.2, y: 0.2 },
-      setXY: { x:400, y: 300 }
-    });
-
-    stars.children.iterate(function (child) {
-      child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-      child.setVelocityX(150 - Math.random() * 300);
-      child.setVelocityY(150 - Math.random() * 300);
-      child.setBounce(1, 1);
-      child.setCollideWorldBounds(true);
-    });
-
     cursors = this.input.keyboard.createCursorKeys();
 
-    box = this.physics.add.image(400, 100, "tiles", 15);
+    flames = this.physics.add.image(400, 200, "fire");
 
-    const processCollision = (box, star) => {
-      star.destroy();
-      const starsLeft = stars.countActive();
-      if (starsLeft === 0) {
-        this.scene.start('winscreen');
-      }
-    }
+    dragonbox = this.physics.add.image(400, 300, "dragon", 15);
+    dragonbox.setBounce(0.2);
+    dragonbox.setCollideWorldBounds(true);
 
-    this.physics.add.collider(
-      stars,
-      box,
-      processCollision,
-      null,
-      this
-    );
-
-
-    box.setBounce(1, 1);
-    box.setCollideWorldBounds(true);
   },
   update: function () {
-    const { velocity } = box.body;
+    const { velocity } = dragonbox.body;
 
     if (cursors.space.isDown) {
       const x = decelerate(velocity.x);
       const y = decelerate(velocity.y);
-      box.setVelocity(x, y)
+      dragonbox.setVelocity(x, y)
     }
 
-    if (cursors.up.isDown) box.setVelocityY(accelerate(velocity.y, -1));
-    if (cursors.right.isDown) box.setVelocityX(accelerate(velocity.x, 1));
-    if (cursors.down.isDown) box.setVelocityY(accelerate(velocity.y, 1));
-    if (cursors.left.isDown) box.setVelocityX(accelerate(velocity.x, -1));
+    if (cursors.up.isDown) dragonbox.setVelocityY(accelerate(velocity.y, -1));
+    if (cursors.right.isDown) dragonbox.setVelocityX(accelerate(velocity.x, 1));
+    if (cursors.down.isDown) dragonbox.setVelocityY(accelerate(velocity.y, 1));
+    if (cursors.left.isDown) dragonbox.setVelocityX(accelerate(velocity.x, -1));
   }
 });
